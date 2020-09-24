956e1e504135d8432aa8a46fff9a84c80779b01d
ciemnicaxdd <57015769+ciemnicaxdd@users.noreply.github.com>
2020-09-24 15:33:20 +0200

    Add files via upload

--- employees.php
+++ employees.php
@@ -5,12 +5,18 @@ $sql="select *from employees ";
 
 $output= $conn-> query($sql);
 
-if($output-> num_rows>0){
-    while($row=$output->fetch_assoc())
-    {
-        echo $row['name'];
-    }
+if($output-> num_rows>0):
+    while($row=$output->fetch_assoc()):
     
-}
+   ?>
+   <table border='1'>
+   <tr>
+   <td><?=$row ['name'] ?></td>
+   </tr>
+   </table>
+    <?php
+    endwhile;
+    endif;
+    ?>
 
-?>
+
